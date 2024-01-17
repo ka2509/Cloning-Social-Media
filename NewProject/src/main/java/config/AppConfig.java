f24242d582dfd124ed136b43cc58eaea7362f4dd
ka2509 <115394338+ka2509@users.noreply.github.com>
2024-01-17 15:13:05 +0700

    Add files via upload

---
+++ NewProject/src/main/java/config/AppConfig.java
@@ -0,0 +1,23 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(Authorize -> Authorize.requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                ).addFilterBefore(, BasicAuthenticationFilter.class)
+
+
+        return null;
+    }
+}
